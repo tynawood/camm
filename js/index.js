f1585011f299525a0c8ddfea4abc92ad027cfe33
Haniel <wood.tyna@gmail.com>
2016-08-23 12:19:52 -0400

    egerge
    gegegegergeger

--- js/index.js
+++ js/index.js
@@ -1,62 +1,56 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  startCamera: function(){
+    CameraPreview.startCamera();
+  },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  startCameraAnotherPos: function(){
+    CameraPreview.startCamera({x: 50, y: 100, width: 300, height:300, camera: "back", tapPhoto: true, previewDrag: true, toBack: false});
+  },
 
-        console.log('Received Event: ' + id);
-    },
+  stopCamera: function(){
+    CameraPreview.stopCamera();
+  },
 
-    takePicture: function() {
-      navigator.camera.getPicture( function( imageURI ) {
-        alert( imageURI );
-      },
-      function( message ) {
-        alert( message );
-      },
-      {
-        quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI
-      });
-    }
+  takePicture: function(){
+    CameraPreview.takePicture({maxWidth: window.device.width, maxHeight: window.device.height});
+  },
+
+  switchCamera: function(){
+    CameraPreview.switchCamera();
+  },
+
+  show: function(){
+    CameraPreview.show();
+  },
+
+  hide: function(){
+    CameraPreview.hide();
+  },
+
+  colorEffectChanged: function(){
+    var effect = document.getElementById('colorEffectCombo').value;
+    CameraPreview.setColorEffect(effect);
+  },
+
+  init: function(){
+    document.getElementById('startCameraButton').addEventListener('click', this.startCamera, false);
+    document.getElementById('startCameraAnotherPosButton').addEventListener('click', this.startCameraAnotherPos, false);
+
+    document.getElementById('stopCameraButton').addEventListener('click', this.stopCamera, false);
+    document.getElementById('takePictureButton').addEventListener('click', this.takePicture, false);
+    document.getElementById('switchCameraButton').addEventListener('click', this.switchCamera, false);
+    document.getElementById('showButton').addEventListener('click', this.show, false);
+    document.getElementById('hideButton').addEventListener('click', this.hide, false);
+    document.getElementById('colorEffectCombo').addEventListener('change', this.colorEffectChanged, false);
+    //window.addEventListener('orientationchange', this.onStopCamera, false);
+    
+    CameraPreview.setOnPictureTakenHandler(function(result){
+      document.getElementById('originalPicture').src = result[0]; //originalPicturePath;
+      document.getElementById('previewPicture').src = result[1]; //previewPicturePath;
+    });
+  }
 };
+
+document.addEventListener('deviceready', function(){	
+  app.init();
+}, false);
